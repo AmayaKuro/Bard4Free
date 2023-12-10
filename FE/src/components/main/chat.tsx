69609cf837c7884b64cbf687e6a50aa38418192f
Kuro <quangacon02@gmail.com>
2023-12-10 22:53:09 +0700

    Make Highlight js run per block"

--- FE/src/components/main/chat.tsx
+++ FE/src/components/main/chat.tsx
@@ -6,6 +6,17 @@ import UserMessage from './response/UserMessage';
 
 import styles from "@/css/main/chat.module.css";
 
+import hljs from "highlight.js/lib/common";
+
+
+const HighLightSyntax: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const highlightedCode = hljs.highlightAuto(children?.toString() || "").value;
+
+    return (
+        <div dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+    );
+}
+
 
 const Chat: React.FC<{ responses: ResponseProps[] }> = ({ responses }) => {
     return (
@@ -15,9 +26,11 @@ const Chat: React.FC<{ responses: ResponseProps[] }> = ({ responses }) => {
                     <UserMessage message={response.message} />
 
                     <Wrapper>
-                        <Markdown>
-                            {response.log}
-                        </Markdown>
+                        <HighLightSyntax>
+                            <Markdown>
+                                {response.log}
+                            </Markdown>
+                        </HighLightSyntax>
                     </Wrapper>
                 </div>
             ))}
