f164791aed12a172426b423b8262998f77e6c909
Kuro <quangacon02@gmail.com>
2023-12-30 12:05:02 +0700

    [Project]: Create Privacy Policy link

--- FE/src/middleware.ts
+++ FE/src/middleware.ts
@@ -15,10 +15,11 @@ export const config = {
       * - _next/image (image optimization files)
       * - favicon.ico (favicon file)
       * - image (image files)
+      * - legacy 
       * - login (login page)
       * - register (register page)
       * - / (the root path)
       */
-        '/((?!_next/static|_next/image|favicon.ico|image|login|register).+)',
+        '/((?!_next/static|_next/image|favicon.ico|image|legacy|login|register).+)',
     ],
 }
